4f58d91cb2b5f77122327b3aacba86b6c13f0fc9
tam95supra-source <tam95.supra@gmail.com>
2026-08-26 17:23:46 +0700

    test(beta77): add compile-only system wrapper stub

---
+++ tools/UiAutomationShellWrapper.java
@@ -0,0 +1,11 @@
+package com.android.commands.uiautomator;
+
+import android.app.UiAutomation;
+
+final class UiAutomationShellWrapper {
+    UiAutomationShellWrapper() {}
+    void connect() {}
+    void disconnect() {}
+    UiAutomation getUiAutomation() { return null; }
+    void setCompressedLayoutHierarchy(boolean compressed) {}
+}
