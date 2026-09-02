6e8b2c02e204dbaddeb0a283711fa144173738bb
tam95supra-source <tam95.supra@gmail.com>
2026-09-03 06:09:43 +0700

    test(beta114): assert current-version changelog content

--- tools/Beta83UiChecksInstrumentation.java
+++ tools/Beta83UiChecksInstrumentation.java
@@ -1164,7 +1164,7 @@ public final class Beta83UiChecksInstrumentation extends Instrumentation {
     waitText("THAY ĐỔI BẢN MỚI",false,false,10000L);
     waitText("Sửa lỗi tài nguyên bản mới",false,false,10000L);
     waitText("THAY ĐỔI BẢN HIỆN TẠI",false,false,10000L);
-    waitText("Quản lý biên bản: chụp ảnh hoặc chọn ảnh từ máy",false,false,10000L);
+    waitText("Thông tin thay đổi được khóa theo đúng phiên bản ứng dụng",false,false,10000L);
     require(findText("SHA256",false,false)==null,"TECHNICAL_RELEASE_METADATA_VISIBLE_IN_CHANGELOG");
     mark("dual_changelog");
     shot(tag+"-07-settings-top");
